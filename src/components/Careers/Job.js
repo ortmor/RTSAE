aaf26ed2b76ec60dc775ce93c09f2f9cfa0f8988
unknown <rekhilelias2014@gmail.com>
2023-07-19 23:32:49 +0530

    updated job imgs

--- src/components/Careers/Job.js
+++ src/components/Careers/Job.js
@@ -8,145 +8,184 @@ const Job = () => {
         <div className={Styles.careersmanagedservicesbox}>
           <div className={Styles.careersmanagedservicescontentmain}>
             <div className={Styles.careersmanagedservicescontentmainboxmain}>
-              <h2>IT Project Manager</h2>
+              <div className={Styles.careersmanagedservicescontentmainboxmainparagraph}>
+              <h2>Sales Account Manager -IT</h2>
+              </div>
+            
+              
               <button>FULL TIME </button>
             </div>
 
             <p>Abu Dhabi, United Arab Emirates</p>
           </div>
-          <h5 className={Styles.linka}>View & Apply</h5>
+          <Link href="/jobs/AM.jpg" target="_blank">
+          <h5 className={Styles.linka}>View & Apply</h5></Link>
         </div>
 
         <div className={Styles.careersmanagedservicesbox}>
           <div className={Styles.careersmanagedservicescontentmain}>
             <div className={Styles.careersmanagedservicescontentmainboxmain}>
+            <div className={Styles.careersmanagedservicescontentmainboxmainparagraph}>
               <h2>Security Operation Manager</h2>
+              </div>
               <button>FULL TIME </button>
             </div>
 
             <p>Abu Dhabi, United Arab Emirates</p>
           </div>
-          <h5 className={Styles.linka}>View & Apply</h5>
+          <Link href="/jobs/RTSJob0723.png" target="_blank">
+          <h5 className={Styles.linka}>View & Apply</h5> </Link>
         </div>
 
         <div className={Styles.careersmanagedservicesbox}>
           <div className={Styles.careersmanagedservicescontentmain}>
             <div className={Styles.careersmanagedservicescontentmainboxmain}>
-              <h2>Service Delivery Manager</h2>
-              <button>FULL TIME </button>
-            </div>
-
-            <p>Abu Dhabi, United Arab Emirates</p>
-          </div>
-          <h5 className={Styles.linka}>View & Apply</h5>
-        </div>
-
-        <div className={Styles.careersmanagedservicesbox}>
-          <div className={Styles.careersmanagedservicescontentmain}>
-            <div className={Styles.careersmanagedservicescontentmainboxmain}>
+            <div className={Styles.careersmanagedservicescontentmainboxmainparagraph}>
               <h2>Network Architecht</h2>
+              </div>
               <button>FULL TIME </button>
             </div>
 
             <p>Abu Dhabi, United Arab Emirates</p>
           </div>
-          <h5 className={Styles.linka}>View & Apply</h5>
+          <Link href="/jobs/RTSJob0723.png" target="_blank">
+          <h5 className={Styles.linka}>View & Apply</h5> </Link>
         </div>
 
         <div className={Styles.careersmanagedservicesbox}>
           <div className={Styles.careersmanagedservicescontentmain}>
             <div className={Styles.careersmanagedservicescontentmainboxmain}>
-              <h2>Sr Governance Consultant ...</h2>
+            <div className={Styles.careersmanagedservicescontentmainboxmainparagraph}>
+              <h2>Sr Governance Consultant (BCP/Risk/ISO)</h2>
+              </div>
               <button>FULL TIME </button>
             </div>
 
             <p>Abu Dhabi, United Arab Emirates</p>
           </div>
-          <h5 className={Styles.linka}>View & Apply</h5>
+          <Link href="/jobs/RTSJob0723.png" target="_blank">
+          <h5 className={Styles.linka}>View & Apply</h5></Link>
         </div>
 
         <div className={Styles.careersmanagedservicesbox}>
           <div className={Styles.careersmanagedservicescontentmain}>
             <div className={Styles.careersmanagedservicescontentmainboxmain}>
-              <h2>Sr Project Manager-Civil</h2>
+            <div className={Styles.careersmanagedservicescontentmainboxmainparagraph}>
+              <h2>Service Delivery Manager</h2>
+              </div>
               <button>FULL TIME </button>
             </div>
 
             <p>Abu Dhabi, United Arab Emirates</p>
           </div>
-          <h5 className={Styles.linka}>View & Apply</h5>
+          <Link href="/jobs/RTSJob0723.png" target="_blank">
+          <h5 className={Styles.linka}>View & Apply</h5> </Link>
         </div>
 
         <div className={Styles.careersmanagedservicesbox}>
           <div className={Styles.careersmanagedservicescontentmain}>
             <div className={Styles.careersmanagedservicescontentmainboxmain}>
-              <h2>Sr Planning Engineer-Civil</h2>
+            <div className={Styles.careersmanagedservicescontentmainboxmainparagraph}>
+              <h2>Sr Presales/Solution Architects- Cyber Security</h2>
+              </div>
+              <button>FULL TIME </button>
+             
+            </div>
+
+            <p>Abu Dhabi, United Arab Emirates</p>
+          </div>
+          <Link href="/jobs/ps.jpg" target="_blank">
+          <h5 className={Styles.linka}>View & Apply</h5> </Link>
+        </div>
+
+        <div className={Styles.careersmanagedservicesbox}>
+          <div className={Styles.careersmanagedservicescontentmain}>
+            <div className={Styles.careersmanagedservicescontentmainboxmain}>
+            <div className={Styles.careersmanagedservicescontentmainboxmainparagraph}>
+              <h2>Sr Presales/Solution Architects- Server & Storage</h2>
+              </div>
               <button>FULL TIME </button>
             </div>
 
             <p>Abu Dhabi, United Arab Emirates</p>
           </div>
-          <h5 className={Styles.linka}>View & Apply</h5>
+          <Link href="/jobs/ps.jpg" target="_blank">
+          <h5 className={Styles.linka}>View & Apply</h5> </Link>
         </div>
 
         <div className={Styles.careersmanagedservicesbox}>
           <div className={Styles.careersmanagedservicescontentmain}>
             <div className={Styles.careersmanagedservicescontentmainboxmain}>
-              <h2>Document Controller-Civil</h2>
+            <div className={Styles.careersmanagedservicescontentmainboxmainparagraph}>
+              <h2>Sr Presales/Solution Architects- Network & Security</h2>
+              </div>
               <button>FULL TIME </button>
             </div>
 
             <p>Abu Dhabi, United Arab Emirates</p>
           </div>
-          <h5 className={Styles.linka}>View & Apply</h5>
+          <Link href="/jobs/ps.jpg" target="_blank">
+          <h5 className={Styles.linka}>View & Apply</h5> </Link>
         </div>
 
         <div className={Styles.careersmanagedservicesbox}>
           <div className={Styles.careersmanagedservicescontentmain}>
             <div className={Styles.careersmanagedservicescontentmainboxmain}>
+            <div className={Styles.careersmanagedservicescontentmainboxmainparagraph}>
               <h2>Data Center Supervisior</h2>
+              </div>
               <button>FULL TIME </button>
             </div>
 
             <p>Abu Dhabi, United Arab Emirates</p>
           </div>
-          <h5 className={Styles.linka}>View & Apply</h5>
+          <Link href="/jobs/RTSJob0723a.png" target="_blank">
+          <h5 className={Styles.linka}>View & Apply</h5> </Link>
         </div>
 
         <div className={Styles.careersmanagedservicesbox}>
           <div className={Styles.careersmanagedservicescontentmain}>
             <div className={Styles.careersmanagedservicescontentmainboxmain}>
-              <h2>Quantity Supervisior</h2>
+            <div className={Styles.careersmanagedservicescontentmainboxmainparagraph}>
+              <h2>Network Security Engineer</h2>
+              </div>
               <button>FULL TIME </button>
             </div>
 
             <p>Abu Dhabi, United Arab Emirates</p>
           </div>
-          <h5 className={Styles.linka}>View & Apply</h5>
+          <Link href="/jobs/nse.png" target="_blank">
+          <h5 className={Styles.linka}>View & Apply</h5> </Link>
         </div>
 
         <div className={Styles.careersmanagedservicesbox}>
           <div className={Styles.careersmanagedservicescontentmain}>
             <div className={Styles.careersmanagedservicescontentmainboxmain}>
-              <h2>Project Coordinator</h2>
+            <div className={Styles.careersmanagedservicescontentmainboxmainparagraph}>
+              <h2>Pre-Sales Engineer (Network and Security)</h2>
+              </div>
               <button>FULL TIME </button>
             </div>
 
             <p>Abu Dhabi, United Arab Emirates</p>
           </div>
-          <h5 className={Styles.linka}>View & Apply</h5>
+          <Link href="/jobs/nse.png" target="_blank">
+          <h5 className={Styles.linka}>View & Apply</h5> </Link>
         </div>
 
         <div className={Styles.careersmanagedservicesbox}>
           <div className={Styles.careersmanagedservicescontentmain}>
             <div className={Styles.careersmanagedservicescontentmainboxmain}>
-              <h2>System Engineer</h2>
+            <div className={Styles.careersmanagedservicescontentmainboxmainparagraph}>
+              <h2>AV Engineer</h2>
+              </div>
               <button>FULL TIME </button>
             </div>
 
             <p>Abu Dhabi, United Arab Emirates</p>
           </div>
-          <h5 className={Styles.linka}>View & Apply</h5>
+          <Link href="/jobs/AVE.png" target="_blank">
+          <h5 className={Styles.linka}>View & Apply</h5> </Link>
         </div>
       </div>
 
