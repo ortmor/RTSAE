a8faae2640679032d6c69024f8cdded74042317b
unknown <rekhilelias2014@gmail.com>
2023-11-23 02:22:40 +0530

    updated sol main pages

--- src/components/DigitalTransformation/Main.js
+++ src/components/DigitalTransformation/Main.js
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import axios from "axios";
 import { useEffect, useState } from "react";
 import Styles from "../../styles/solution.module.scss";
 import { Fragment } from "react";
@@ -8,14 +9,16 @@ import { Fragment } from "react";
 const Main = () => {
   const ApiPoint = process.env.API_KEY;
   const [data, setData] = useState([]);
-
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(ApiPoint + "/en/solution/public");
-        const result = await response.json();
-
-        setData(result.results);
+        const response = await axios.get(ApiPoint + "/en/solution/public");
+        const allData = response.data.results;
+        const filteredData = allData.filter(
+          (item) => item.type === "DIGITAL_TRANSFORMATION"
+        );
+        setData(filteredData);
+        setMain(filteredData[0]?.solution || {});
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -28,54 +31,60 @@ const Main = () => {
     <Fragment>
       <br />
       <br />
-      {data.map((sol, index) => {
-        if (sol.type === "DIGITAL_TRANSFORMATION") {
-          const isEven = index % 2 === 0;
+      {data.map((sol, index) => (
+        <div key={sol.id}>
+          {index % 2 === 0 ? (
+            <div className={Styles.icthomemainparentdivthree}>
+              <div className={Styles.icthomemainboxthree}>
+                <img src={sol.image} alt={sol.title}></img>
+                <div className={Styles.icthomemainboxthreeimg}></div>
+              </div>
 
-          return (
-            <div key={sol.id}>
-              {isEven ? (
-                <div className={Styles.icthomemainparentdivthree}>
-                  <div className={Styles.icthomemainboxthree}>
-                    <img src={sol.image} alt={sol.title}></img>
-                    <div className={Styles.icthomemainboxthreeimg}></div>
-                  </div>
-
-                  <div className={Styles.icthomemainboxthree}>
-                    <div className={Styles.icthomemainboxthreeheading}>
-                      <h1>{sol.title} </h1>
-                    </div>
-                    <br />
-                    <Link href={`/digitaltransformation/${sol.id}`}>
-                      {" "}
-                      Readmore{" "}
-                    </Link>
-                  </div>
+              <div className={Styles.icthomemainboxthree}>
+                <div className={Styles.icthomemainboxthreeheading}>
+                  <h1
+                    dangerouslySetInnerHTML={{
+                      __html:
+                        typeof sol.title === "string"
+                          ? sol.title.replace(/\n/g, "<br/>")
+                          : sol.title,
+                    }}
+                  />
                 </div>
-              ) : (
-                <div className={Styles.icthomemainparentdiv}>
-                  <div className={Styles.icthomemainboxone}>
-                    <div className={Styles.icthomemainboxoneheading}>
-                      <h1>{sol.title}</h1>
-                    </div>
-                    <br />
-                    <Link href={`/digitaltransformation/${sol.id}`}>
-                      {" "}
-                      Readmore{" "}
-                    </Link>
-                  </div>
-                  <div className={Styles.icthomemainboxtwo}>
-                    <img src={sol.image} alt={sol.title}></img>
-                    <div className={Styles.icthomemainboxtwoimg}></div>
-                  </div>
-                </div>
-              )}
+                <br />
+                <Link href={`/digitaltransformation/${sol.id}`}>
+                  {" "}
+                  Readmore{" "}
+                </Link>
+              </div>
             </div>
-          );
-        } else {
-          return null;
-        }
-      })}
+          ) : (
+            <div className={Styles.icthomemainparentdiv}>
+              <div className={Styles.icthomemainboxone}>
+                <div className={Styles.icthomemainboxoneheading}>
+                  <h1
+                    dangerouslySetInnerHTML={{
+                      __html:
+                        typeof sol.title === "string"
+                          ? sol.title.replace(/\n/g, "<br/>")
+                          : sol.title,
+                    }}
+                  />
+                </div>
+                <br />
+                <Link href={`/digitaltransformation/${sol.id}`}>
+                  {" "}
+                  Readmore{" "}
+                </Link>
+              </div>
+              <div className={Styles.icthomemainboxtwo}>
+                <img src={sol.image} alt={sol.title}></img>
+                <div className={Styles.icthomemainboxtwoimg}></div>
+              </div>
+            </div>
+          )}
+        </div>
+      ))}
     </Fragment>
   );
 };
